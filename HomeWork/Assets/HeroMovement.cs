00a08b4fe8c8ec1f7e1f695a716aa478e0a16a3e
mikepogorelov <fh279@yandex.ru>
2020-03-08 13:47:36 +0300

    implementing "Jumping" function. In process.

--- HomeWork/Assets/HeroMovement.cs
+++ HomeWork/Assets/HeroMovement.cs
@@ -4,96 +4,30 @@ using UnityEngine;
 
 public class HeroMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-   
-    // Start is called before the first frame update
+    public float speed = 0.1f;
+    public float gravity = -0.5f;
+    public float jumppower = 3f;
+    public float JumpTimer = 1;
     public void Start()
     {
     
-
-
-
-
     }
-
-    // Update is called once per frame
     public void Update()
 
-    {
-        var position = Input.GetAxis("Horizontal");
-        float movementSpeed = 3;
+{
+    var position = Input.GetAxis("Horizontal");
+    transform.position = transform.position + new Vector3(position, gravity, 0)*Time.deltaTime;
 
-        switch (position)
+        if (JumpTimer > 0 && Input.GetButton("Jump"))
         {
-            case position.:
-            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
-            case position > 0:
-            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+            JumpTimer -= Time.deltaTime;
+            transform.position = transform.position + new Vector3(0, jumppower, 0) * Time.deltaTime;
         }
-        
-        
-        //if (position < 0)
-        //{ transform.Translate(Vector3.left * movementSpeed * Time.deltaTime); }
-        //else if (position > 0)
-        //{ transform.Translate(Vector3.right * movementSpeed * Time.deltaTime); }
-
-        
-
-        //if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
-        
-        //}
-        //else if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
-        //}
-        
-        
-        
-        
-        
-        
-        
-        //}
-        //if (Input.GetButtonDown("UpArrow")) 
-        //{
-        //    rb.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 3f), ForceMode2D.Impulse);
-        //    Input.GetAxis("Horizontal");
-        //}
-
-        //float horizontal = Input.GetAxis("Horizontal")*5;
-        //float vertical = Input.GetAxis("Vertical") * 5;
-        //Debug.Log(horizontal);
-        //HandleMovement(horizontal);
-        //jump();
-        //HandleMovement(vertical);
-        //if (horizontal < 0)
-        //{
-        //m_FacingRight = false;
-        //transform.localRotation = Quaternion.Euler(0, 180, 0);
-        //}
-        //else
-        //{
-        //m_FacingRight = true;
-        //transform.localRotation = Quaternion.Euler(0, 0, 0);
-        //}
-
-
-    }
-    //private void HandleMovement(float horizontal)
-    //{
-    //myRigidbody.velocity = new Vector2(horizontal, myRigidbody.velocity.y);
-    //myRigidbody.velocity = new Vector2(vertical, myRigidbody.velocity.y);
-    //}
-    //void jump()
-    //{
-    //if (Input.GetButtonDown("Jump"))
-    //{
-    //gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 3f), ForceMode2D.Impulse);
-    //}
-
-    //}
+        else 
+            JumpTimer = 1 ;
+        transform.position = transform.position + new Vector3(0, gravity, 0) * Time.deltaTime;
+    }    
+    
 
 }
 
