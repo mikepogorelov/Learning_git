988a97ac0ddc011d11437dd05aa1c8aa34836727
mikepogorelov <fh279@yandex.ru>
2020-03-10 08:13:55 +0300

    it can jump, but not correctly

--- HomeWork/Assets/HeroMovement.cs
+++ HomeWork/Assets/HeroMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class HeroMovement : MonoBehaviour
 {
     public float speed = 0.1f;
-    public float gravity = -0.5f;
-    public float jumppower = 3f;
+    public float gravity = 0.5f;
+    public float jumpSpeed = 3f;
     public float JumpTimer = 1;
     public void Start()
     {
@@ -16,16 +16,33 @@ public class HeroMovement : MonoBehaviour
 
 {
     var position = Input.GetAxis("Horizontal");
-    transform.position = transform.position + new Vector3(position, gravity, 0)*Time.deltaTime;
+    transform.position = transform.position + new Vector3(position, -gravity, 0)*Time.deltaTime;
 
-        if (JumpTimer > 0 && Input.GetButton("Jump"))
+        if (transform.position.y <= -1.5)
         {
-            JumpTimer -= Time.deltaTime;
-            transform.position = transform.position + new Vector3(0, jumppower, 0) * Time.deltaTime;
+            gravity = 0;
         }
-        else 
-            JumpTimer = 1 ;
-        transform.position = transform.position + new Vector3(0, gravity, 0) * Time.deltaTime;
+        
+        
+        if ( Input.GetButton("Jump")) //jumpSpeed > gravity &&
+        {
+            gravity = 0.5f;
+            JumpTimer -= Time.deltaTime;
+            transform.position = transform.position + new Vector3(0, jumpSpeed, 0) * Time.deltaTime;
+            
+        }
+        //else
+        //    JumpTimer = 1; 
+        //transform.position = transform.position + new Vector3(0, gravity, 0) * Time.deltaTime;
+
+        //while (JumpTimer >= 0 && Input.GetButton("Jump"))
+        //{
+        //    JumpTimer -= Time.deltaTime;
+        //    transform.position = transform.position + new Vector3(0, jumpSpeed, 0) * Time.deltaTime;
+
+        //}
+
+
     }    
     
 
