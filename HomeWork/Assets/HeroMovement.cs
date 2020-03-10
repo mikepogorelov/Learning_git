778ff36d5e971e8002d78e6c9162d0b6a6d63d68
mikepogorelov <fh279@yandex.ru>
2020-03-10 21:46:01 +0300

    next stage

--- HomeWork/Assets/HeroMovement.cs
+++ HomeWork/Assets/HeroMovement.cs
@@ -4,47 +4,35 @@ using UnityEngine;
 
 public class HeroMovement : MonoBehaviour
 {
-    public float speed = 0.1f;
-    public float gravity = 0.5f;
-    public float jumpSpeed = 3f;
-    public float JumpTimer = 1;
+    public float jump_height_modifier;
+    public Vector3 speed;
+    public Vector3 gravity;
+    
+    
     public void Start()
     {
-    
+
     }
     public void Update()
+    {
+        var position = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3(position*speed.x, gravity.y, 0) * Time.deltaTime;
 
-{
-    var position = Input.GetAxis("Horizontal");
-    transform.position = transform.position + new Vector3(position, -gravity, 0)*Time.deltaTime;
-
-        if (transform.position.y <= -1.5)
-        {
-            gravity = 0;
-        }
-        
-        
-        if ( Input.GetButton("Jump")) //jumpSpeed > gravity &&
-        {
-            gravity = 0.5f;
-            JumpTimer -= Time.deltaTime;
-            transform.position = transform.position + new Vector3(0, jumpSpeed, 0) * Time.deltaTime;
-            
-        }
-        //else
-        //    JumpTimer = 1; 
-        //transform.position = transform.position + new Vector3(0, gravity, 0) * Time.deltaTime;
-
-        //while (JumpTimer >= 0 && Input.GetButton("Jump"))
-        //{
-        //    JumpTimer -= Time.deltaTime;
-        //    transform.position = transform.position + new Vector3(0, jumpSpeed, 0) * Time.deltaTime;
-
-        //}
+        if (transform.position.y <= -1.5f)
+            {
+                transform.position = transform.position + new Vector3(0, -gravity.y, 0) * Time.deltaTime;
+            }
+        if (Input.GetButtonDown("Jump"))
+            {
+                transform.position = transform.position + new Vector3(0, speed.y * jump_height_modifier, 0);
+            }
 
 
-    }    
-    
+
+
+
+    }
+
 
 }
 
