f86d620cc8a0666805ffe611541d98610d2ff6d7
mikepogorelov <fh279@yandex.ru>
2020-03-10 22:42:55 +0300

    blyat

--- HomeWork/Assets/HeroMovement.cs
+++ HomeWork/Assets/HeroMovement.cs
@@ -4,36 +4,35 @@ using UnityEngine;
 
 public class HeroMovement : MonoBehaviour
 {
-    public float jump_height_modifier;
     public Vector3 speed;
     public Vector3 gravity;
-    
-    
+        
     public void Start()
     {
 
     }
     public void Update()
     {
+
         var position = Input.GetAxis("Horizontal");
-        transform.position = transform.position + new Vector3(position*speed.x, gravity.y, 0) * Time.deltaTime;
+        transform.position = transform.position + new Vector3(position * speed.x, gravity.y, 0) * Time.deltaTime;
 
         if (transform.position.y <= -1.5f)
-            {
-                transform.position = transform.position + new Vector3(0, -gravity.y, 0) * Time.deltaTime;
-            }
-        if (Input.GetButtonDown("Jump"))
-            {
-                transform.position = transform.position + new Vector3(0, speed.y * jump_height_modifier, 0);
-            }
-
-
-
+        {
+            transform.position = transform.position + new Vector3(0, -gravity.y, 0) * Time.deltaTime;
+        }
+        if (Input.GetButtonDown("Jump") && transform.position.y <= -1f)
+        {
+            transform.position = transform.position + new Vector3(0, speed.y, 0) * Time.deltaTime;
+        }
+        Debug.Log(Time.deltaTime);
 
 
     }
 
-
 }
+
+
+
 
 
