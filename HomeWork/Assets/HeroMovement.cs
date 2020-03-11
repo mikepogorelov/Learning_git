7ad3f2dff7c2ca619ba41952fdc8b3b65341c424
Pavel Loginov <pavel.loginov@melscience.com>
2020-03-11 19:11:33 +0300

    blyat 2

--- HomeWork/Assets/HeroMovement.cs
+++ HomeWork/Assets/HeroMovement.cs
@@ -6,33 +6,29 @@ public class HeroMovement : MonoBehaviour
 {
     public Vector3 speed;
     public Vector3 gravity;
-        
+    public float smth;
+
     public void Start()
     {
-
     }
+
     public void Update()
     {
-
         var position = Input.GetAxis("Horizontal");
         transform.position = transform.position + new Vector3(position * speed.x, gravity.y, 0) * Time.deltaTime;
 
         if (transform.position.y <= -1.5f)
         {
-            transform.position = transform.position + new Vector3(0, -gravity.y, 0) * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, -1.5f);
+            //transform.position = transform.position + -gravity * Time.deltaTime;
         }
-        if (Input.GetButtonDown("Jump") && transform.position.y <= -1f)
+
+
+        if (Input.GetButtonDown("Jump") && transform.position.y <= -1.5f)
         {
             transform.position = transform.position + new Vector3(0, speed.y, 0) * Time.deltaTime;
         }
-        Debug.Log(Time.deltaTime);
 
-
+        
     }
-
-}
-
-
-
-
-
+} 
