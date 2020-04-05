3f7f6fb9dae01b5545122d5be64e14fcd8c7ea80
mikepogorelov <fh279@yandex.ru>
2020-04-05 13:32:57 +0300

    it works. This version was sent to S.Skorbyakov

--- Assets/HeroMovement.cs
+++ Assets/HeroMovement.cs
@@ -1,34 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
 public class HeroMovement : MonoBehaviour
 {
-    public Vector3 speed;
-    public Vector3 gravity;
-    public float smth;
+    //(посмотри, сначала я ввожу default_jump_force_value(присваиваю значение в редакторе), затем current_jump_force_value. Произвожу какие-то действия с current_jump_force_value, а затем мне нужно чтобы значение переменной стало дефолтным. И без default_jump_force_value я хз как это сделать.
+    // ------------------------------------|- \
+    public float default_gravity_value;//    |   --     КАК ИЗБЕЖАТЬ           ТАКИХ
+    public float default_jump_force_value;// |   --                  ВВЕДЕНИЯЕ        ПЕРЕМЕННЫХ?
+    // ------------------------------------|- / 
+    public float speed_value;
+    float gravity_value;
+    
 
-    public void Start()
-    {
-    }
-
+    float current_jump_force_value=0;
+    
     public void Update()
-    {
-        var position = Input.GetAxis("Horizontal");
-        transform.position = transform.position + new Vector3(position * speed.x, gravity.y, 0) * Time.deltaTime;
+    {   // Каждую фичу (ходьба, гравитация, прыжок) я поделил на "модули" (вектор, что_делает_фича, доп_условия). Это нормально или сначала прописывают векторы, потом логику, потом доп условия? 
+        //////////////////////////////// HORIZONTAL MOVEMENT IMPLEMENTATION ///////////////////////////////////////////// 
 
-        if (transform.position.y <= -1.5f)
-        {
-            transform.position = new Vector3(transform.position.x, -1.5f);
-            //transform.position = transform.position + -gravity * Time.deltaTime;
-        }
+        float horizontal_momevent_direction = Input.GetAxis("Horizontal");  // -1...+1
+        Vector3 horizontal_motion_vector = new Vector3(speed_value*horizontal_momevent_direction, 0.0f, 0.0f) * Time.deltaTime;
+        transform.position = transform.position + horizontal_motion_vector;
 
-
-        if (Input.GetButtonDown("Jump") && transform.position.y <= -1.5f)
-        {
-            transform.position = transform.position + new Vector3(0, speed.y, 0) * Time.deltaTime;
-        }
-
+        ///////////////////////////////////// GRAVITY IMPLEMENTATION ////////////////////////////////////////////////////
+                
+        Vector3 gravity_motion_vector = new Vector3(0.0f, gravity_value, 0.0f) * Time.deltaTime;
+        transform.position = transform.position + gravity_motion_vector;
         
+        if (transform.position.y <= 0f)
+        {
+            gravity_value = 0;
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else
+        {
+            gravity_value -= 0.5f; //Подобрал экспериментально. 
+        }
+
+        //////////////////////////////////// JUMPING IMPLEMENTATION  ////////////////////////////////////////////////////
+        
+        Vector3 jump_direction_vector = new Vector3(0.0f, current_jump_force_value, 0.0f) * Time.deltaTime;
+        transform.position = transform.position + jump_direction_vector;
+        if (current_jump_force_value > 0f)
+        {
+            current_jump_force_value -= 2f; //Подобрал экспериментально.
+        }
+        else
+        {
+            current_jump_force_value = 0f;
+        }
+        if (Input.GetButtonDown("Jump") && transform.position.y <= 0f)
+        {
+            current_jump_force_value = default_jump_force_value;
+        }
     }
 } 
