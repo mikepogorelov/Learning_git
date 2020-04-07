73f53e471c1f1871555594c1dec974226bf07e68
mikepogorelov <fh279@yandex.ru>
2020-04-07 10:20:57 +0300

    bad but stable

--- Assets/HeroMovement.cs
+++ Assets/HeroMovement.cs
@@ -2,22 +2,19 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class HeroMovement : MonoBehaviour
 {
-    //(посмотри, сначала я ввожу default_jump_force_value(присваиваю значение в редакторе), затем current_jump_force_value. Произвожу какие-то действия с current_jump_force_value, а затем мне нужно чтобы значение переменной стало дефолтным. И без default_jump_force_value я хз как это сделать.
-    // ------------------------------------|- \
-    public float default_gravity_value;//    |   --     КАК ИЗБЕЖАТЬ           ТАКИХ
-    public float default_jump_force_value;// |   --                  ВВЕДЕНИЯЕ        ПЕРЕМЕННЫХ?
-    // ------------------------------------|- / 
+    
+    
+    public float default_gravity_value;
+    public float default_jump_force_value;
     public float speed_value;
     float gravity_value;
-    
-
     float current_jump_force_value=0;
     
     public void Update()
-    {   // Каждую фичу (ходьба, гравитация, прыжок) я поделил на "модули" (вектор, что_делает_фича, доп_условия). Это нормально или сначала прописывают векторы, потом логику, потом доп условия? 
+    {
         //////////////////////////////// HORIZONTAL MOVEMENT IMPLEMENTATION ///////////////////////////////////////////// 
 
-        float horizontal_momevent_direction = Input.GetAxis("Horizontal");  // -1...+1
+        float horizontal_momevent_direction = Input.GetAxis("Horizontal");  
         Vector3 horizontal_motion_vector = new Vector3(speed_value*horizontal_momevent_direction, 0.0f, 0.0f) * Time.deltaTime;
         transform.position = transform.position + horizontal_motion_vector;
 
@@ -33,7 +30,7 @@ public class HeroMovement : MonoBehaviour
         }
         else
         {
-            gravity_value -= 0.5f; //Подобрал экспериментально. 
+            gravity_value -= 0.5f; 
         }
 
         //////////////////////////////////// JUMPING IMPLEMENTATION  ////////////////////////////////////////////////////
@@ -42,7 +39,7 @@ public class HeroMovement : MonoBehaviour
         transform.position = transform.position + jump_direction_vector;
         if (current_jump_force_value > 0f)
         {
-            current_jump_force_value -= 2f; //Подобрал экспериментально.
+            current_jump_force_value -= 2f; 
         }
         else
         {
